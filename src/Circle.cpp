a8b734de865b7add1ee35ed7fc526a9b8bab746b
Armorjin <armorcoon@gmail.com>
2023-09-05 23:20:34 +0300

    test

--- src/Circle.cpp
+++
@@ -1,8 +0,0 @@
-#include "Circle.h"
-
-Circle::Circle (double aR){
- r=aR;
-}
-double Circle:: calcSquare(){
-    return M_PI*r*r;
-}
--- src/Rectangle.cpp
+++
@@ -1,9 +0,0 @@
-#include "Rectangle.h"
-
-Rectangle::Rectangle (double mA,double mB){
-    a=mA;
-    b=mB;
-}
-double Rectangle:: calcSquare(){
-    return a*b;
-}
